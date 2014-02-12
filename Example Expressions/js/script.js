16583cd549667864fdccc208811e507c7f8aea6b
bryanjgill <bryanjgill@gmail.com>
2014-02-11 21:03:13 -0500

    Example Expressions 2

--- Example Expressions/js/script.js
+++ Example Expressions/js/script.js
@@ -1,6 +1,5 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2; //sets up our variable a and defines it with 2
-var b;
-b = a + 3; //add 3 to a
-console.log (b);
+var yearBorn = 1975;
+var age = 2014 - yearBorn;
+console.log (age);
