9087a39c019dcd0749b63ff2bd0965e596d22464
bryanjgill <bryanjgill@gmail.com>
2014-02-13 19:55:50 -0500

    Update Personal

--- Expressions Assignment/Personal/js/script.js
+++ Expressions Assignment/Personal/js/script.js
@@ -3,21 +3,20 @@
 //	Assignment:	Expressions Assignment - Personal Calculations
 
 //	Server Memory Calculator
-var numberOfProcessors = 2;
-var slotsPerProcessor = 9;
-var desiredMemory = 64;
+var numberOfProcessors = prompt("How many processors are in your server?");
+var slotsPerProcessor = prompt("How many memory slots per processor?");
+var desiredMemory = prompt("How much memory would you like to upgrade to?");
 
 var totalSlots = (numberOfProcessors * slotsPerProcessor);
 var twox = (desiredMemory/2);
 var fourx = (desiredMemory/4);
-var sixx = (desiredMemory/6);
 var eightx = (desiredMemory/8);
 var sixteenx = (desiredMemory/16);
 
 console.log("To reach " + desiredMemory + "GB of RAM in your server,");
 console.log("with " + totalSlots + " total slots and memory installed in pairs,");
 console.log("you can install the following memory configurations:");
-console.log("2x "+ twox +"GB sticks.");
-console.log("4x "+ fourx +"GB sticks.");
-console.log("8x "+ eightx +"GB sticks.");
-console.log("16x "+ sixteenx +"GB sticks.");
+if (totalSlots >= 2) console.log("2x "+ twox +"GB sticks.");
+if (totalSlots >= 4) console.log("4x "+ fourx +"GB sticks.");
+if (totalSlots >= 8) console.log("8x "+ eightx +"GB sticks.");
+if (totalSlots >= 16) console.log("16x "+ sixteenx +"GB sticks.");
