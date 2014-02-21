5232e8d68d623d0a8d0cdd45a3cd04414532b16a
bryanjgill <bryanjgill@gmail.com>
2014-02-20 20:04:18 -0500

    Wacky commit 1

--- Conditionals Assignment/Conditionals-Wacky/js/script.js
+++ Conditionals Assignment/Conditionals-Wacky/js/script.js
@@ -2,63 +2,60 @@
 //	Date:				02/18/2014
 //	Assignment:	Conditionals Assignment - Wacky
 
-//  
+//  Guess until you get you get the number
 
 // prompt user to input variables
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
-var = prompt("");  //  
+prompt("Welcome to the guess it 'til you get it game!");  //Display welcome message
+var name = prompt("Please enter your first name.");  //  Prompt user for their first name
+var userGuess1 = prompt("Please enter your guess:");  //  Prompt user for their first guess
+var randomNumber = Math.floor((Math.random()*10)+1);
+var numberOne = 1;
+var numberTwo = 2;
+var numberThree = 3;
+var numberFour = 4;
+var numberFive = 5;
+var numberSix = 6;
+var numberSeven = 7;
+var numberEight = 8;
+var numberNine = 9;
+var numberTen = 10;
 
-//  Calculate
-var = parse()  parse();  //  
-var = parse()  parse();  //  
-var = parse()  parse();  //  
-var = parse()  parse();  //  
-var = parse()  parse();  //  
-var = parse()  parse();  //  
-
-//  Calculate
-var = parse()  parse();  //  
-var = parse()  parse();  //  
-
-//  Print all variables to console to verify calculations
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-//  console.log();
-
-//  Determine
-if(){  //  
-	console.log();  //  Print message to console
-	console.log();  //  Print message to console
-}else if{  //  
-	console.log();  //  Print message to console
-	console.log();  //  Print message to console
-}else if{  //  
-	console.log();  //  Print message to console
-	console.log();  //  Print message to console
-}else if{  //  
-	console.log();  //  Print message to console
-	console.log();  //  Print message to console
+//  Determine if user guessed the correct number or prompt again
+if(parseInt(userGuess1) > parseInt(randomNumber)){  //  Determine if user's number is greater than or equal to 5
+	var userGuess2 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess3 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess4 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess5 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess6 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess7 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess8 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+	var userGuess9 = prompt("Please guess higher number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess10 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess11 = prompt("Please guess lower number:");	
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess12 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess13 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess14 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess15 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess16 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess17 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+	var userGuess18 = prompt("Please guess lower number:");
+}else if(parseInt(userGuess1) == parseInt(randomNumber)){  //  
+	console.log("You chose wisely!");  //  Print message to console
+	console.log("Your guess: " +userNumber+ "Random number: " +randomNumber+ ".");  //  Print message to console
 }
