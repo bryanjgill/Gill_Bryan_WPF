aed001608221605d8eca9626da99ad1f9119459d
bryanjgill <bryanjgill@gmail.com>
2014-02-20 20:15:12 -0500

    Wacky attempt

--- Conditionals Assignment/Conditionals-Wacky/js/script.js
+++ Conditionals Assignment/Conditionals-Wacky/js/script.js
@@ -5,7 +5,7 @@
 //  Guess until you get you get the number
 
 // prompt user to input variables
-prompt("Welcome to the guess it 'til you get it game!");  //Display welcome message
+alert("Welcome to the guess it 'til you get it game!");  //Display welcome message
 var name = prompt("Please enter your first name.");  //  Prompt user for their first name
 var userGuess1 = prompt("Please enter your guess:");  //  Prompt user for their first guess
 var randomNumber = Math.floor((Math.random()*10)+1);
@@ -23,37 +23,37 @@ var numberTen = 10;
 //  Determine if user guessed the correct number or prompt again
 if(parseInt(userGuess1) > parseInt(randomNumber)){  //  Determine if user's number is greater than or equal to 5
 	var userGuess2 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess3 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess4 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess5 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess6 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess7 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess8 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 > parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) > parseInt(randomNumber)){  //  
 	var userGuess9 = prompt("Please guess higher number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess10 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess11 = prompt("Please guess lower number:");	
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess12 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess13 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess14 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess15 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess16 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess17 = prompt("Please guess lower number:");
-}else if(parseInt(userGuess2 < parseInt(randomNumber)){  //  
+}else if(parseInt(userGuess2) < parseInt(randomNumber)){  //  
 	var userGuess18 = prompt("Please guess lower number:");
 }else if(parseInt(userGuess1) == parseInt(randomNumber)){  //  
 	console.log("You chose wisely!");  //  Print message to console
